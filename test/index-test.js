0f339a19f7040fffffd002dc011b1f8b23a59c91
dervishiamela@gmail.com Dervishi <dervishiamela@gmail.com>
2018-03-29 22:00:22 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -4,14 +4,14 @@ var index = require("../index.js")
 describe('what-is-a-test', () => {
   describe('Name', () => {
     it('returns "JOE"', () => {
-      expect(index.name).toEqual('Susan')
+      expect(index.name).toEqual('JOE')
     })
   })
 
 
   describe('Height', () => {
     it('is more than 40', () => {
-      expect(index.height).toBeLessThan(40)
+      expect(index.height).toBeMoreThan(40)
     })
   })
 
